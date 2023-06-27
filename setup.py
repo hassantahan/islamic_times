38e1a3226d04cff7fc2fded2d6f39abe8800b893
HassanTahan <contact@hassantahan.com>
2023-06-27 12:13:33 -0400

    publishing

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='islamic_times',
+    version='1.0.0',
+    description='Various calculations for Islamic purposes',
+    author='Hassan Tahan',
+    author_email='contact@hassantahan.com',
+    url='https://github.com/hassantahan/islamic_times',
+    packages=find_packages(),
+    install_requires=[
+        'numpy', 'timezonefinder', 'pytz', 'datetime', 'math'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
