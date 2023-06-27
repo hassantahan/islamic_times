acb13d0deff88d4caf2020d693b6ae17e40b71a3
HassanTahan <contact@hassantahan.com>
2023-06-26 21:20:11 -0400

    package completed

--- islamic_times.py
+++ islamic_times.py
@@ -12,41 +12,54 @@ class ITLocation:
         self.latitude = latitude
         self.longitude = longitude
         self.elev = elev
-    
-        ### Find UTC Offset According to Lat/Long & adjust datetime
-        today = datetime.datetime.utcnow()
-        tz_name, utc_diff = find_utc_offset(latitude, longitude, today)
-        today += datetime.timedelta(hours=utc_diff)
-        utc_diff *= -1
+        self.update()
 
+    def update(self):
+        ### Find UTC Offset According to Lat/Long & adjust datetime
+        self.today = datetime.datetime.utcnow()
+        self.tz_name, self.utc_diff = find_utc_offset(self.latitude, self.longitude, self.today)
+        self.today += datetime.timedelta(hours=self.utc_diff)
+        self.utc_diff *= -1
 
         ### Calculate Julian Date
-        jd = gregorian_to_jd(today.year, today.month, today.day + fraction_of_day(today), -1 * utc_diff)
+        self.jd = gregorian_to_jd(self.today.year, self.today.month, self.today.day + fraction_of_day(self.today), -1 * self.utc_diff)
 
         ### Sun & Moon Properties Calculations
         # Get factors arrays
-        sun_factors = sunpos(jd, latitude, longitude)
-        delPsi, delEps = sun_nutation(jd)
-        moon_factors = moonpos(jd, latitude, longitude, delPsi, sun_factors[13], elev)
+        self.sun_factors = sunpos(self.jd, self.latitude, self.longitude)
+        self.delPsi, self.delEps = sun_nutation(self.jd)
+        self.moon_factors = moonpos(self.jd, self.latitude, self.longitude, self.delPsi, self.sun_factors[13], self.elev)
 
         # Important Sun Factors placed into local variables
-        sun_declination = sun_factors[11]
-        alpha_hour = sun_factors[10] // 15
-        alpha_minute = int((sun_factors[10] / 15 - alpha_hour) * 60)
-        alpha_second = (sun_factors[10] / 15 * 60 - alpha_hour * 60 - alpha_minute) * 60
-        solar_angle = solar_hour_angle(latitude, sun_declination)
-        sun_alt = sun_factors[15]
-        sun_az = sun_factors[16]
+        self.sun_declination = self.sun_factors[11]
+        self.sun_alpha = decimal_to_hms(self.sun_factors[10])
+        self.solar_angle = solar_hour_angle(self.latitude, self.sun_declination)
+        self.sun_alt = self.sun_factors[15]
+        self.sun_az = self.sun_factors[16]
 
         # Important Moon Factors placed into local variables
-        moon_declination = moon_factors[7]
-        moon_alpha = decimal_to_hms(moon_factors[6])
-        moon_alt = moon_factors[9]
-        moon_az = moon_factors[10]
+        self.moon_declination = self.moon_factors[7]
+        self.moon_alpha = decimal_to_hms(self.moon_factors[6])
+        self.moon_alt = self.moon_factors[9]
+        self.moon_az = self.moon_factors[10]   
 
+        return
+
+    def datetime(self):
+        ### Calculate Current Islamic Date (estimate)
+        # TODO: Look into newer versions of this, see if it can be corrected.
+        islamic_date = gregorian_to_hijri(self.today.year, self.today.month, self.today.day)
+
+        return {"gregorian" : self.today.strftime("%A, %d %B, %Y"), 
+                "hijri" : f"{get_islamic_day(self.today.strftime('%A'))}, {islamic_date[2]} {get_islamic_month(islamic_date[1])}, {islamic_date[0]}",
+                "time" : self.today.strftime("%X"), "timezone" : self.tz_name, "utc_offset" : format_utc_offset(self.utc_diff * -1),
+                "eq_of_time" : np.round(equation_of_time(self.jd, self.latitude, self.longitude), 2)}
 
     def prayertimes(self):
         ### Prayer Time Calculations
+        # Equation of Time
+        eq_of_time = equation_of_time(self.jd, self.latitude, self.longitude)
+
         # Calculate prayer times in solar time
         solar_fajr = sunrise_sunset(-1, solar_hour_angle(self.latitude, self.sun_declination, self.FAJR_ANGLE))
         solar_sunrise = sunrise_sunset(-1, self.solar_angle)
@@ -55,16 +68,98 @@ class ITLocation:
         solar_isha = sunrise_sunset(1, solar_hour_angle(self.latitude, self.sun_declination, self.ISHA_ANGLE))
 
         # Convert prayer times from solar to standard time
-        standard_fajr = solar2standard(solar_fajr, self.utc_diff, self.longitude, self.eq_of_time)
-        standard_sunrise = solar2standard(solar_sunrise, self.utc_diff, self.longitude, self.eq_of_time)
-        standard_noon = solar2standard(12.0, self.utc_diff, self.longitude, self.eq_of_time)
+        standard_fajr = solar2standard(solar_fajr, self.utc_diff, self.longitude, eq_of_time)
+        standard_sunrise = solar2standard(solar_sunrise, self.utc_diff, self.longitude, eq_of_time)
+        standard_noon = solar2standard(12.0, self.utc_diff, self.longitude, eq_of_time)
         standard_asr = standard_noon + asr_time(self.latitude, self.sun_declination)
-        standard_sunset = solar2standard(solar_sunset, self.utc_diff, self.longitude, self.eq_of_time)
-        standard_maghrib = solar2standard(solar_maghrib, self.utc_diff, self.longitude, self.eq_of_time)
-        standard_isha = solar2standard(solar_isha, self.utc_diff, self.longitude, self.eq_of_time)
-        standard_midnight = time_midpoint(standard_sunset, find_tomorrow_fajr(self.jd, self.utc_diff, self.latitude, self.longitude, self.eq_of_time, self.FAJR_ANGLE))
+        standard_sunset = solar2standard(solar_sunset, self.utc_diff, self.longitude, eq_of_time)
+        standard_maghrib = solar2standard(solar_maghrib, self.utc_diff, self.longitude, eq_of_time)
+        standard_isha = solar2standard(solar_isha, self.utc_diff, self.longitude, eq_of_time)
+        standard_midnight = time_midpoint(standard_sunset, find_tomorrow_fajr(self.jd, self.utc_diff, self.latitude, self.longitude, eq_of_time, self.FAJR_ANGLE))
 
-        return [standard_fajr, standard_sunrise, standard_noon, standard_asr, standard_sunset, standard_maghrib, standard_isha, standard_midnight]
-            
-
+        return {"fajr" : float_to_24time(standard_fajr), "sunrise" : float_to_24time(standard_sunrise), "noon" : float_to_24time(standard_noon), 
+                "asr" : float_to_24time(standard_asr), "sunset" : float_to_24time(standard_sunset), "maghrib" : float_to_24time(standard_maghrib), 
+                "isha" : float_to_24time(standard_isha), "midnight" : float_to_24time(standard_midnight)}
     
+    def mecca(self):
+        mecca_distance, mecca_direction = haversine(self.latitude, self.longitude, self.MECCA_LAT, self.MECCA_LONG)
+        mecca_direction = bound_angle_deg(mecca_direction)
+        
+        return {"distance" : np.round(mecca_distance, 2), "angle" : np.round(mecca_direction, 2), "cardinal" : get_cardinal_direction(np.round(mecca_direction))}
+    
+    def sun(self):
+        return {"declination" : f"{self.sun_declination:.3f}°", "right_ascension" : f"{self.sun_alpha[0]}h {self.sun_alpha[0]}m {self.sun_alpha[0]:.2f}s",
+                "altitude" : f"{self.sun_alt:.2f}°", "azimuth" : f"{self.sun_az:.2f}°"}
+    
+    def moon(self):
+        ### Calculate Moon Illumination
+        moon_illumin = moon_illumination(self.sun_declination, self.sun_factors[10], self.sun_factors[4], 
+                                            self.moon_declination, self.moon_factors[6], self.moon_factors[1], 
+                                            self.moon_factors[0], self.sun_factors[6], self.moon_factors[2] / 149597870.7)
+
+        return {"declination" : f"{self.moon_declination:.3f}°", "right_ascension" : f"{self.moon_alpha[0]}h {self.moon_alpha[1]}m {self.moon_alpha[2]:.2f}s",
+                "altitude" : f"{self.moon_alt:.2f}°", "azimuth" : f"{self.moon_az:.2f}°", "illumination" : f"{moon_illumin * 100:.1f}%"}
+    
+    def moonphases(self):
+        ### Find Next New Moon (and the rest of the phases)
+        moon_phases = next_phases_of_moon_utc(self.today)
+        for i, phase in enumerate(moon_phases):
+            phase_str = ""
+            if i == 0:
+                phase_str = "New Moon"
+            elif i == 1:
+                phase_str = "First Quarter"
+            elif i == 2:
+                phase_str = "Full Moon"
+            else:
+                phase_str = "Last Quarter"
+            
+            moon_phases[i] = {"phase": phase_str, "datetime": phase - datetime.timedelta(hours=self.utc_diff)}
+
+        moon_phases = sorted(moon_phases, key = lambda item: item["datetime"])
+
+        return moon_phases
+
+    def visibilities(self):
+        ### Calculate New Moon Visibilities
+        
+        # Get New Moon Date from moon_phases list
+        moon_phases = self.moonphases()
+        for item in moon_phases:
+                if item['phase'] == "New Moon":
+                    new_moon = item['datetime']
+
+        # Find JD for the given date; adjust day for difference in UTC and local timezone
+        jd_new_moon = gregorian_to_jd(new_moon.year, new_moon.month, new_moon.day + fraction_of_day(new_moon), -1 * self.utc_diff)
+        if new_moon.day != jd_to_gregorian(jd_new_moon).day:
+            if new_moon.day < jd_to_gregorian(jd_new_moon).day:
+                new_moon += datetime.timedelta(days=1)
+            else:
+                new_moon -= datetime.timedelta(days=1)
+            jd_new_moon = gregorian_to_jd(new_moon.year, new_moon.month, new_moon.day, -1 * self.utc_diff)
+
+        # Find local sunset as visibilities are calculated from then
+        nm_sun_factors = sunpos(jd_new_moon, self.latitude, self.longitude)
+        nm_sunset = solar2standard(sunrise_sunset(1, solar_hour_angle(self.latitude, nm_sun_factors[11])), self.utc_diff, self.longitude, equation_of_time(jd_new_moon, self.latitude, self.longitude))
+        jd_new_moon = gregorian_to_jd(new_moon.year, new_moon.month, new_moon.day + nm_sunset / 24, -1 * self.utc_diff)
+
+        # Find visibilities for the three days
+        visibilities = []
+        i = 0
+        while (i < 3):
+            nm_sun_factors = sunpos(jd_new_moon + i, self.latitude, self.longitude)
+            delPsi, delEps = sun_nutation(jd_new_moon + i)
+            nm_moon_factors = moonpos(jd_new_moon + i, self.latitude, self.longitude, delPsi, nm_sun_factors[13], self.elev)
+
+            #print(jd_to_gregorian(jd_new_moon + i))
+            visibilities.append(calculate_visibility(nm_sun_factors[16], nm_sun_factors[15], nm_moon_factors[10], nm_moon_factors[9], np.deg2rad(nm_moon_factors[8])))
+            i += 1
+
+        # Arrange and classify visibilties
+        q_values = [
+            [visibilities[0], classify_visibility(visibilities[0])],
+            [visibilities[1], classify_visibility(visibilities[1])],
+            [visibilities[2], classify_visibility(visibilities[2])],
+        ]
+
+        return {"0" : q_values[0], "1" : q_values[1], "2" : q_values[2]}
--- test.py
+++ test.py
@@ -1,9 +1,59 @@
-from hijri_converter import convert
+import islamic_times
 
-# Gregorian date to convert
-gregorian_date = '2023-06-23'
+##### Definitions #####
+TO_LAT 	= 43.74506
+TO_LONG = -79.30947
+TO_ELEV = 170.5
 
-# Convert Gregorian to Hijri
-hijri_date = convert.Gregorian(gregorian_date).to_hijri()
+##### Inputs #####
+latitude = TO_LAT #37.336111
+longitude = TO_LONG #-121.890556
+elev = TO_ELEV #25
 
-print(hijri_date)
+##### Calculation #####
+it = islamic_times.ITLocation(TO_LAT, TO_LONG, TO_ELEV)
+
+##### Outputs #####
+# Date & Time
+print("Time & Date\n\tGregorian Date:\t\t{}".format(it.datetime()["gregorian"]))
+print(f"\tIslamic Date:\t\t{it.datetime()['hijri']}")
+print("\t24h-Time:\t\t{}\n\tTime Zone:\t\t{} {}".format(it.datetime()["time"], it.datetime()["timezone"], it.datetime()["utc_offset"]))
+print("\tEquation of time:\t{} minutes".format(it.datetime()["eq_of_time"]))
+
+# Prayer Times
+print("Prayer Times\n\tFajr:\t\t\t{}".format(it.prayertimes()["fajr"]))
+print("\tSunrise:\t\t{}".format(it.prayertimes()["sunrise"]))
+print("\tẒuhr:\t\t\t {}".format(it.prayertimes()["noon"]))
+print("\t‘Asr:\t\t\t{}".format(it.prayertimes()["asr"]))
+print("\tSunset: \t\t{}".format(it.prayertimes()["sunset"]))
+print("\tMaghrib: \t\t{}".format(it.prayertimes()["maghrib"]))
+print("\t‘Isha: \t\t\t{}".format(it.prayertimes()["isha"]))
+print("\tMidnight: \t\t{}".format(it.prayertimes()["midnight"]))
+
+# Mecca
+print("Mecca\n\tDistance: \t\t{} km".format(it.mecca()["distance"]))
+print("\tDirection: \t\t{} ({}°)".format(it.mecca()["cardinal"], it.mecca()["angle"]))
+
+# The Sun
+print("The Sun\n\tApp. Declination:\t{}".format(it.sun()["declination"]))
+print("\tApp. Right Ascenscion:\t{}".format(it.sun()["right_ascension"]))
+print("\tAltitude:\t\t{}".format(it.sun()["altitude"]))
+print("\tAzimuth:\t\t{}".format(it.sun()["azimuth"]))
+
+# The Moon
+print("The Moon\n\tApp. Declination:\t{}".format(it.moon()["declination"]))
+print("\tApp. Right Ascenscion:\t{}".format(it.moon()["right_ascension"]))
+print("\tAltitude:\t\t{}".format(it.moon()["altitude"]))
+print("\tAzimuth:\t\t{}".format(it.moon()["azimuth"]))
+print("\tIllumination:\t\t{}".format(it.moon()["illumination"]))
+
+# Moon Phases
+print("Moon Phases\n\t{}:\t\t{}".format(it.moonphases()[0]["phase"], it.moonphases()[0]["datetime"].strftime("%H:%M:%S %A, %d %B, %Y")))
+print("\t{}:\t\t{}".format(it.moonphases()[1]["phase"], it.moonphases()[1]["datetime"].strftime("%H:%M:%S %A, %d %B, %Y")))
+print("\t{}:\t\t{}".format(it.moonphases()[2]["phase"], it.moonphases()[2]["datetime"].strftime("%H:%M:%S %A, %d %B, %Y")))
+print("\t{}:\t\t{}".format(it.moonphases()[3]["phase"], it.moonphases()[3]["datetime"].strftime("%H:%M:%S %A, %d %B, %Y")))
+
+# New Moon Visibility
+print("Visibility Values of New Moon after\n\t0 days:\t\t\t{:.3f} ({})".format(it.visibilities()["0"][0], it.visibilities()["0"][1]))
+print("\t1 day:\t\t\t{:.3f} ({})".format(it.visibilities()["1"][0], it.visibilities()["1"][1]))
+print("\t2 days:\t\t\t{:.3f} ({})".format(it.visibilities()["2"][0], it.visibilities()["2"][1]))
