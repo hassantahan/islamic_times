1e519312f648e2b17226048b6e555ee993817d63
HassanTahan <contact@hassantahan.com>
2023-08-13 14:32:56 -0400

    version number update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='islamic_times',
-    version='1.0.1',
+    version='1.0.2',
     description='Various calculations for Islamic purposes',
     readme = "README.md",
     author='Hassan Tahan',
